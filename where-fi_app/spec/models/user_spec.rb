ef40f3572f16227759f9f9dfc5066d9452ebbaf1
CaptainM <caroline.a.morgan@vanderbilt.edu>
2014-11-19 16:26:06 -0500

    We deleted the test file and added rspec. We wrote validation tests for the User model. We created the six User routes and views. Functional. We added full_name to the User model. We added Bcrypt to the Gemfile.

---
+++ where-fi_app/spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe User do 
+
+	it { is_expected.to validate_presence_of :username}
+	it { is_expected.to validate_uniqueness_of :username}
+	it { is_expected.to validate_presence_of :first_name}
+	it { is_expected.to validate_presence_of :last_name}
+	it { is_expected.to validate_presence_of :password}
+
+	let(:hari) {new_user = User.new( username: "superman",
+	first_name: "hari",
+	last_name: "Doe"
+	password: "hunter2",
+	password_confirmation: "hunter2")}
+
+
+	it "is valid with a username, first_name, last_name and password" do
+		expect(hari).to be_valid
+	end
+
+	
+end
