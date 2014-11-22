beaf5982ea7e02bb40a46c84bd395f8d5e0a0991
bigtone1284 <anthony.defreitas@gmail.com>
2014-11-22 18:26:34 -0500

    revised some rspec files.

--- where-fi_app/app/models/user.rb
+++ where-fi_app/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name, :username
   validates_uniqueness_of :username
+  validates :first_name, :last_name, :numericality => false 
   has_secure_password
 
   has_and_belongs_to_many :locations
--- where-fi_app/spec/models/location_spec.rb
+++ where-fi_app/spec/models/location_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
 describe Location do 
-	it { is_expected.to validate_presence_of :address}
-	it { is_expected.to validate_presence_of :ssid}
-	it { is_expected.to validate_presence_of :place_name}
+	it { is_expected.to validate_presence_of :ssid }
+	it { is_expected.to validate_presence_of :place_name }
 
 
-	let(:valid_location) {new_location = Location.new(
-		address: "506 Amsterdam Avenue NY, NY 10024",
+	let(:valid_location) { new_location = Location.create(
+		address: nil,
 		ssid: "Guest",
 		place_name: "My Mommas house",
 		boro: "SI",
 		details: "Second floor gets best signal",
-		lat: "42.9043",
-		long: "-72.8394"
+		latitude: "42.9043",
+		longitude: "-72.8394"
 		)}
 
-	it "is valid with address, ssid, and place_name" do 
+	it "is valid ssid and place_name" do 
 		expect(valid_location).to be_valid
 	end
 end
